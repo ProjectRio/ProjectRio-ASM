8d6c8b71f03543cc16e2f50b918561739dcf53a9
LittleCoaks <LittleCoaks@users.noreply.github.com>
2022-12-19 21:44:25 -0500

    Update StorePortInfo.asm

--- Required Project Rio Codes/StorePortInfo.asm
+++ Required Project Rio Codes/StorePortInfo.asm
@@ -14,31 +14,36 @@
 ###########################################################
 # Grab Active Ports
 
-# Inject: 0x80042CD0
+# Inject: 0x80042CD8
 START:
-  lis r5, 0x802E                # Load init port P1 addr
-  ori r5, r5, 0xBF91
-  lis r3, 0x8089                # Load P1 CPU bool
-  ori r3, r3, 0x2ACA
-  lbz r3, 0(r3)
-  cmpwi r3, 0x1                 # Is P1 CPU
+  stwu r1, -0x50 (r1)            # Backup stack, make r1ace for 18 registers
+  stmw r14, 0x8 (r1)
+
+  lis r15, 0x802E                # Load init port P1 addr
+  ori r15, r15, 0xBF91
+  lis r16, 0x8089                # Load P1 CPU bool
+  ori r16, r16, 0x2ACA
+  lbz r16, 0(r16)
+  cmpwi r16, 0x1                 # Is P1 CPU
   beq- SET_P1_CPU
-  li r3, 0x1                    # If not CPU, set to 1 (P1)
+  li r16, 0x1                    # If not CPU, set to 1 (P1)
   b SET_P2
 
 SET_P1_CPU:
-  li r3, 0x5
+  li r16, 0x5
 
 SET_P2:
-  stb r3, 0(r5)                 # Store P1 info to r5
-  lis r3, 0x800E                # Get Px's port
-  ori r3, r3, 0x874D
-  lbz r3, 0(r3)                 
-  addi r3, r3, 0x1              # Add 1 since we want P1 == 1 instead of 0
-  stb r3, 1(r5)                 # Store Px info
-  li r3, 0x1B8                  # Replace overwritten instruction
+  stb r16, 0(r15)                # Store P1 info to r5
+  lis r16, 0x800E                # Get Px's port
+  ori r16, r16, 0x874D
+  lbz r16, 0(r16)                 
+  addi r16, r16, 0x1             # Add 1 since we want P1 == 1 instead of 0
+  stb r16, 1(r15)                # Store Px info
 
 END:
+  lmw r14, 0x8 (r1)
+  addi r1, r1, 0x50              # restore stack
+  li r5, 0x7f                    # Replace overwritten instruction
 
 
 
