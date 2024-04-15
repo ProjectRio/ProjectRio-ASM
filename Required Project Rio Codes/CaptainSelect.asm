f9eb8f5814fb4ddbaffc957f8f684a117758b92e
LittleCoaks <LittleCoaks@users.noreply.github.com>
2024-04-14 21:37:47 -0400

    Fix Stars not staying after swap
    Cursor auto-moves but that's the only issue

--- Required Project Rio Codes/CaptainSelect.asm
+++ Required Project Rio Codes/CaptainSelect.asm
@@ -1,31 +1,31 @@
 ###########################################################
-# Pause Clock
+# Captain Swap
 ###########################################################
-# Author: nuche
-# 
-# Writes:
-# These two lines must be added to the end of the code to nop the instructions moved to within the injected code.
-# 0464f678 60000000
-# 0464f67c 60000000
+# Author: nuche, LittleCoaks
 
-# Inject 0x8064f674
-START:
+
+###########################################################
+###########################################################
+# Requires: "2064F67C 40820AB4"
+
+# Inject 0x8064f67c
+Start:
   lis r11,0x800f 			# Checks if on main menu rel
   subi r12,r11,0x7884 
   lhzu r10, 0 (r12) 
   cmplwi r10, 4 
-  beq STARTMOD 				# if main menu rel, then branch to the mod.
+  beq StartMod 				# if main menu rel, then branch to the mod.
 
 #If not main menu, then replace normal match instructions from injection site
   addi	r6, r4, 10636
   li r5, 13
   addi r4, r3, 2320
-  b END 
+  b End 
 
-STARTMOD:
+StartMod:
   rlwinm. r0,r30,0,19,19 		# check if start button is pressed.
   rlwinm r3,r30,0,16,31
-  beq END 
+  beq End 
 
 # Pre-checks before switching captains.
   lis r11,0x8075 			# Check if showing player profile
@@ -33,14 +33,14 @@ STARTMOD:
   add r11,r12,r27
   lbzu r10,0x45(r11)
   cmplwi r10, 0
-  bne BADSOUND 
+  bne BadSound 
 
   lis r11, 0x8075			# Check if cursor is on bottom half of screen
   addi r12, r11, 3144
   add r11, r12, r27
   lbzu r10, 0x41 (r11)
   cmplwi r10, 0
-  bne BADSOUND
+  bne BadSound
 
   lis r11, 0x8075			# Check if the cursor isn't over a captain character.
   addi r12, r11, 0x0c48
@@ -48,7 +48,7 @@ STARTMOD:
   add r11, r12, r10
   lwzu r8, 0X00 (r11)
   cmpwi r8, 0x9
-  bge BADSOUND
+  bge BadSound
 
 # Find roster spot of the cursor position, in case it's swapped.
   lis r11, 0x803c			# roster mapping address
@@ -58,17 +58,17 @@ STARTMOD:
   li r10, 0x9
   mtctr r10				# store loop index
   li r7, 0x0
-LOOP1START:
+Loop1Start:
   lbz r10, 0 (r11)
   cmpw r8, r10				# compare mapping with cursor pos
-  bne LOOP1END 				# if not equal, then try the next index
-  b MAPFOUND 				# if equal, then found the mapping
-LOOP1END: 
+  bne Loop1End 				# if not equal, then try the next index
+  b MapFound 				# if equal, then found the mapping
+Loop1End: 
   addi r11, r11, 0x1 			# add 1 to mapping mem address
   addi r7, r7, 0x1 			# add 1 to the roster spot
-  bdnz LOOP1START
-  b BADSOUND 				# cursor position not found
-MAPFOUND:
+  bdnz Loop1Start
+  b BadSound 				# cursor position not found
+MapFound:
   nop 					# r7 now holds the roster spot of the desired captain 
 
 # Get CharID of cursor position
@@ -84,16 +84,16 @@ MAPFOUND:
   subi r12, r11, 0x7130
   li r11, 0xc				# set loop limit of 12
   mtctr r11
-LOOP2START:
+Loop2Start:
   lbz r11, 0 (r12) 			# load captain from list
   cmpw r9, r11 				# compare new captain with captain list
-  bne LOOP2END
-  b CAPFOUND
-LOOP2END:
+  bne Loop2End
+  b CapFound
+Loop2End:
   addi r12, r12, 0x1 			# add 1 to captain list index
-  bdnz LOOP2START
-  b BADSOUND #captain not found
-CAPFOUND:
+  bdnz Loop2Start
+  b BadSound #captain not found
+CapFound:
   nop					# r9 now holds charID of new captain 
 
 # Change cap ID 
@@ -140,18 +140,18 @@ CAPFOUND:
   lis r9, 0x803c
   addi r9, r9,0x674b 			# start of chem with captain array (after captain)
   add r9, r9, r10 			# add team offset
-LOOP3START:
+Loop3Start:
   lbz r7, 0(r12) 			# get char ID of roster spot
   cmpwi		r7, 0xff 		# see if roster spot is empty.
-  beq		LOOP3END 		# if roster spot empty, go to end to prepare for new loop
+  beq		Loop3End 		# if roster spot empty, go to end to prepare for new loop
 # If roster spot not empty
   add r7, r7, r11 			# store address of chem with captain into r7
   lbz r8, 0 (r7) 			# store chem into r8
   stb r8, 0(r9) 			# store chem (r8) into team chem address (r9)
-LOOP3END:
+Loop3End:
   addi r9, r9, 0x1 			# update chem array address by 1
   addi r12, r12, 1 			# increment roster char ID addresses
-  bdnz LOOP3START 			# increment loop counter and go back to start
+  bdnz Loop3Start 			# increment loop counter and go back to start
 
 # Call the team logo function
   mflr r0
@@ -186,23 +186,40 @@ LOOP3END:
   addi r1,r1,0x100
   mtlr r0
 
+  # update state var
+UpdateStateVar:
+  mflr r0 				# backup stack frame
+  stw r0, 0x4(r1)
+  stwu r1,-0x100(r1)
+  stmw r3,0x8(r1)
+  or r3, r27, r27   # set params
+  li r4, 0xf
+  lis r12, 0x8006 			# call function
+  ori r12, r12, 0x25A4
+  mtctr r12
+  bctrl
+  lmw r3,0x8(r1) 			# restore stack frame
+  lwz r0, 0x104(r1)
+  addi r1,r1,0x100
+  mtlr r0
+
 # Call sound effects
-GOODSOUND:
+GoodSound:
   mflr r0 				# backup stack frame
   stw r0, 0x4(r1)
   stwu r1, -0x100(r1)
   stmw r3, 0x8(r1)
   li r3, 0x1bc 				# set param 1 (sound code)
-  b PLAYSOUND
+  b PlaySound
 
-BADSOUND:
+BadSound:
   mflr r0 				# backup stack frame
   stw r0, 0x4(r1)
   stwu r1, -0x100(r1)
   stmw r3, 0x8(r1)
   li r3, 0x1ba 				# set param 1 (sound code)
 
-PLAYSOUND:
+PlaySound:
   lis r4, 0x800e 			# set param 2 (volume)
   ori r4, r4, 0xfba4
   lwz r4, 0(r4)
@@ -217,5 +234,4 @@ PLAYSOUND:
   addi r1, r1,0x100
   mtlr r0
 
-END:
-  nop
+End:
